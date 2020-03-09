30218904cb43a0d9be0565f931a078c297ebd8e0
Artem Gavrilov <artem.gavrilov@percona.com>
2020-03-09 17:55:52 +0200

    PMM-5540 Fix references in license headers

--- config/http_client.go
+++ config/http_client.go
@@ -13,7 +13,7 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-// Original repository: https://github.com/prometheus/prometheus
+// Original repository: https://github.com/prometheus/common
 
 // Copyright 2015 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
