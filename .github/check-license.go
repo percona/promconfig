70e59493397ea314d20d7b950750ea0a5b66b8ac
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-03-18 18:10:33 +0300

    PMM-5540 Update license.

--- .github/check-license.go
+++ .github/check-license.go
@@ -1,21 +1,24 @@
 // promconfig
-//Copyright 2020 Percona LLC
+// Copyright 2020 Percona LLC
 //
-//Licensed under the Apache License, Version 2.0 (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at
+// Based on Prometheus systems and service monitoring server.
+// Copyright 2015 The Prometheus Authors.
 //
-//http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build ignore
 
-// check-license checks that AGPL license header in all files matches header in this file.
+// check-license checks that license header in all files matches header in this file.
 package main
 
 import (
