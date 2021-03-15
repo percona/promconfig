2b423cd81650a5c5b76ebc1b1888cd39b1ff226b
Girish Kumar <45126881+zkmrgirish@users.noreply.github.com>
2021-03-15 18:44:51 +0530

    Extend ServiceDiscoveryConfig for other platforms (#8)
    * Major cloud provider service discovery configurations
    
    * extend service discovery configs
    
    - digitalocean
    - dockerswarm
    - dns_sd_config
    - Consul
    
    * testdata changes for extra sd configs
    
    * Test case for ec2 and gce configs

--- service_discovery.go
+++ service_discovery.go
@@ -26,6 +26,20 @@ type ServiceDiscoveryConfig struct {
 	FileSDConfigs []*FilesSDConfig `yaml:"file_sd_configs,omitempty"`
 	// List of Kubernetes service discovery configurations.
 	KubernetesSDConfigs []*KubernetesSDConfig `yaml:"kubernetes_sd_configs,omitempty"`
+	// List of AWS EC2 service discovery configurations.
+	EC2SDConfigs []*EC2SDConfig `yaml:"ec2_sd_configs,omitempty"`
+	// List of Google cloud GCE service discovery configurations.
+	GceSDConfigs []*GceSDConfig `yaml:"gce_sd_configs,omitempty"`
+	// List of azure cloud service discovery configurations.
+	AzureSDConfigs []*AzureSDConfig `yaml:"azure_sd_configs,omitempty"`
+	// List of digitalocean droplet service discovery configurations.
+	DigitaloceanSDConfigs []*DigitaloceanSDConfig `yaml:"digitalocean_sd_configs,omitempty"`
+	// List of consul cataloge service discovery configurations.
+	ConsulSDConfigs []*ConsulSDConfig `yaml:"consul_sd_configs,omitempty"`
+	// List of docker swarm service discovery configurations.
+	DockerswarmSDConfigs []*DockerswarmSDConfig `yaml:"dockerswarm_sd_configs,omitempty"`
+	// List of dns-based service discovery configurations.
+	DNSSDConfigs []*DNSSDConfig `yaml:"dns_sd_configs,omitempty"`
 }
 
 // Group is a set of targets with a common label set(production , test, staging etc.).
@@ -49,4 +63,86 @@ type KubernetesSDConfig struct {
 	Role               string           `yaml:"role"`
 	HTTPClientConfig   HTTPClientConfig `yaml:",inline"`
 	NamespaceDiscovery []string         `yaml:"namespaces,omitempty"`
+}
+
+// EC2SDConfig is the configuration for AWS EC2 instance service discovery.
+type EC2SDConfig struct {
+	Region          string    `yaml:"region,omitempty"`
+	Endpoint        string    `yaml:"endpoint,omitempty"`
+	AccessKey       string    `yaml:"access_key,omitempty"`
+	SecretKey       string    `yaml:"secret_key,omitempty"`
+	Profile         string    `yaml:"profile,omitempty"`
+	RoleArn         string    `yaml:"role_arn,omitempty"`
+	RefreshInterval Duration  `yaml:"refresh_interval,omitempty"`
+	Port            int       `yaml:"port,omitempty"`
+	Filters         []*Filter `yaml:"filters,omitempty"`
+}
+
+// GceSDConfig is the configuration for Google cloud GCE instance service discovery
+type GceSDConfig struct {
+	Project         string   `yaml:"project"`
+	Zone            string   `yaml:"zone"`
+	Filter          string   `yaml:"filter,omitempty"`
+	RefreshInterval Duration `yaml:"refresh_interval,omitempty"`
+	Port            int      `yaml:"port,omitempty"`
+	TagSeprator     string   `yaml:"tag_separator,omitempty"`
+}
+
+// AzureSDConfig is the configuration for Azure cloud service discovery
+type AzureSDConfig struct {
+	Environment     string   `yaml:"environment,omitempty"`
+	SubscriptionID  string   `yaml:"subscription_id"`
+	TenantID        string   `yaml:"tenant_id,omitempty"`
+	ClientID        string   `yaml:"client_id,omitempty"`
+	ClientSecret    string   `yaml:"client_secret,omitempty"`
+	RefreshInterval Duration `yaml:"refresh_interval,omitempty"`
+	Port            int      `yaml:"port,omitempty"`
+}
+
+// DigitaloceanSDConfig is the configuration for digitalocean droplet service discovery
+type DigitaloceanSDConfig struct {
+	HTTPClientConfig HTTPClientConfig `yaml:",inline"`
+	RefreshInterval  Duration         `yaml:"refresh_interval,omitempty"`
+	Port             int              `yaml:"port,omitempty"`
+}
+
+// ConsulSDConfig is the configuration for the consul cataloge service discovery
+type ConsulSDConfig struct {
+	Server          string            `yaml:"server,omitempty"`
+	Token           string            `yaml:"token"`
+	Datacenter      string            `yaml:"datacenter"`
+	Scheme          string            `yaml:"scheme,omitempty"`
+	Username        string            `yaml:"username"`
+	Password        string            `yaml:"password"`
+	TLSConfig       TLSConfig         `yaml:"tls_config,omitempty"`
+	Services        []string          `yaml:"services,omitempty"`
+	Tags            []string          `yaml:"tags,omitempty"`
+	NodeMeta        map[string]string `yaml:"node_meta,omitempty"`
+	TagSeprator     string            `yaml:"tag_seprator,omitempty"`
+	AllowStale      bool              `yaml:"allow_stale"`
+	RefreshInterval Duration          `yaml:"refresh_interval,omitempty"`
+}
+
+// DockerswarmSDConfig is the configuration for service discovery of docker services, tasks or nodes.
+type DockerswarmSDConfig struct {
+	HTTPClientConfig HTTPClientConfig `yaml:",inline"`
+	Host             string           `yaml:"host"`
+	Role             string           `yaml:"role"`
+	RefreshInterval  Duration         `yaml:"refresh_interval,omitempty"`
+	Port             int              `yaml:"port,omitempty"`
+	Filters          []*Filter        `yaml:"filters,omitempty"`
+}
+
+// DNSSDConfig is configuration for dns based service discovery.
+type DNSSDConfig struct {
+	Names           []string `yaml:"names"`
+	Type            string   `yaml:"type,omitempty"`
+	RefreshInterval Duration `yaml:"refresh_interval,omitempty"`
+	Port            int      `yaml:"port,omitempty"`
+}
+
+// Filter to limit service discovery
+type Filter struct {
+	Name   string   `yaml:"name"`
+	Values []string `yaml:"values"`
 }
