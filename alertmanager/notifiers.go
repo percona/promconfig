e521ef3a23a2cf4d7540d25fe408b41957972312
Andrew Minkin <andrew.minkin@percona.com>
2022-07-13 13:16:45 +0600

    PMM-10159 added dots

--- alertmanager/notifiers.go
+++ alertmanager/notifiers.go
@@ -196,7 +196,7 @@ type OpsGenieConfig struct {
 	Priority    string                    `yaml:"priority,omitempty"`
 }
 
-// OpsGenieConfigResponder returns configuration of OpsGenie responder
+// OpsGenieConfigResponder returns configuration of OpsGenie responder.
 type OpsGenieConfigResponder struct {
 	// One of those 3 should be filled.
 	ID       string `yaml:"id,omitempty"`
@@ -242,7 +242,7 @@ type PushoverConfig struct {
 	HTML     bool              `yaml:"html"`
 }
 
-// SNSConfig configures notifications via SNS
+// SNSConfig configures notifications via SNS.
 type SNSConfig struct {
 	NotifierConfig `yaml:",inline"`
 
